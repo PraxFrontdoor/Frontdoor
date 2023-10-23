fac5991b5a7a105f4f80e28a0f45bd004a3d863a
NathanHershey <86255838+NathanHershey@users.noreply.github.com>
2023-10-23 17:27:54 -0500

    Changed testscript.js code

--- nathan/testscript.js
+++ nathan/testscript.js
@@ -1,1 +1,5 @@
-document.writeln('Hello, World!');
+    var message = "Hi there! Click OK to continue.";
+    alert(message);
+     
+    /* The following line won't execute until you dismiss previous alert */
+    alert("This is another alert box.");
